7d1228c4f5a865b3693ab28866845ca75c91f700
Naeem Ahmed <naeemahmed7860@gmail.com>
2024-09-13 12:33:53 +0100

    Removed GitHub link for portfolio website project and updated descriptions for the Finance Dashboard App and other projects. Also updated Stats

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -26,23 +26,21 @@ const projects = [
     category: "Fullstack",
     title: "Portfolio Website",
     description:
-      "This portfolio isn’t just a showcase—it’s my coding adventure. Built with Next.js and Tailwind CSS, it’s the result of countless hours tweaking things to look and feel just right. It’s still evolving as I learn new tricks. And hey, you’re already here, so I guess it’s doing its job!",
+      "This portfolio isn’t just a showcase—it’s my coding adventure. Built with Next.js and Tailwind CSS, it’s the result of countless hours tweaking things to look and feel just right. It’s still evolving as I learn new tricks. And hey, you’re already here, so I guess it’s doing its job! If you'd like more insight into how this was built or have any questions, feel free to reach out.",
     stack: [
       { name: "Next.js" },
       { name: "Tailwind CSS" },
       { name: "Framer Motion" },
     ],
     image: "/computer.png",
-    //github: "https://github.com/naeem0512/Portfolio",
+    // github: "https://github.com/naeem0512/naeem-portfolio-website",
   },
   {
     num: "02",
     category: "Fullstack",
     title: "Finance Dashboard App",
     description:
-      `I’ve always had a thing for finance—there’s something exciting about turning raw data into meaningful insights. So, I built the Finance Dashboard App, a tool that not only tracks Revenue, Profit, and Expenses but also uses machine learning to predict future trends. It’s like having a finance guru in your pocket!
-      
-      P.S. If your company could use a little financial wizardry, hmu ;)`,
+      `I’ve always had a thing for finance—there’s something exciting about turning raw data into meaningful insights. So, I built the Finance Dashboard App, a tool that not only tracks Revenue, Profit, and Expenses but also uses machine learning to predict future trends. It’s like having a financial expert at your fingertips, delivering real-time insights and predictive trends right when you need them.`,
     stack: [
       { name: "MongoDB" },
       { name: "Express.js" },
@@ -59,7 +57,7 @@ const projects = [
     category: "Frontend",
     title: "Pathfinding Visualiser",
     description:
-      "This project emerged while I was preparing for my university data structures and algorithms exams. To solidify my understanding, I built the Pathfinding Visualiser, bringing algorithms like Dijkstra and A* to life on screen. It not only helped me excel in my exams but also sharpened my programming skills, creating a tool that could benefit others in their learning journey.",
+      "This project emerged while I was preparing for my university data structures and algorithms exams. To solidify my understanding, I built the Pathfinding Visualiser, bringing algorithms like Dijkstra and A* to life on screen.",
     stack: [
       { name: "React" },
       { name: "TypeScript" },
@@ -69,138 +67,41 @@ const projects = [
     github: "https://github.com/naeem0512/pathfinding-visualiser",
     live: "https://pathfinding-visualiser-kappa.vercel.app/",
   },
-  {
-    num: "04",
-    category: "Python",
-    title: "Sign Language Detection",
-    description:
-      "This project is close to my heart as it blends technology with social impact. I developed a sign language detection system using Python and machine learning to bridge communication gaps for sign language users. It highlighted the importance of accessibility in tech and was deeply fulfilling to create something that could genuinely make a difference.",
-    stack: [
-      { name: "Python" },
-      { name: "TensorFlow" },
-    ],
-    image: "/signlang.jpeg",
-    github: "https://github.com/naeem0512/Sign-Language",
-  },
-  {
-    num: "05",
-    category: "Machine Learning",
-    title: "Emotion Classification in Images: Happy vs. Sad",
-    description:
-      "This project is a simple yet effective introduction to the world of neural networks and deep learning. It focuses on classifying emotions in images as either 'Happy' or 'Sad' using a neural network model. The goal is to accurately (or nearly accurately) predict the emotion in the given images.",
-    stack: [
-      { name: "Python" },
-      { name: "Jupyter Notebook" },
-      { name: "TensorFlow" },
-    ],
-    image: "/cnn-image.png",  
-    github: "https://github.com/naeem0512/cnn-image-classifier",
-    live: "https://drive.google.com/file/d/1aFuYjcCnIL7I9EOkq4Q2041F-KTmCqlB/view?usp=sharing",
-  },
-  {
-    num: "06",
-    category: "Backend",
-    title: "Luxury Wheels",
-    description:
-      "Luxury Wheels was a project that challenged my backend development skills in a significant way. The idea was to create a specialized web-based car rental system for luxury and vintage cars, targeting high-end wedding events. I aimed to design a seamless booking interface that would provide an exceptional user experience. Throughout this project, I honed my PHP and MySQL skills while also understanding the importance of user experience in backend systems. It was rewarding to see how a robust backend can elevate the entire product.",
-    stack: [
-      { name: "PHP" },
-      { name: "MySQL" },
-      { name: "JavaScript" },
-    ],
-    image: "/l.png",
-    github: "https://github.com/naeem0512/LuxuryWheels",
-  },
-  {
-    num: "07",
-    category: "Frontend",
-    title: "Phaser Brick Breaker Game",
-    description:
-      "Building this Phaser-based brick breaker game was a nostalgic blast from the past. The challenge? Capture that classic, addictive fun with my own twist. It took me deep into game dev with HTML, JavaScript, and Phaser, and reminded me why I love coding—it’s all about creating something that brings joy!",
-    stack: [
-      { name: "HTML" },
-      { name: "JavaScript" },
-      { name: "Phaser" },
-    ],
-    image: "/bricks.png",
-    github: "https://github.com/naeem0512/Phaser-Brick-Breaker-Game",
-    live: "https://phaser-brick-breaker-game.vercel.app/",
-  },
-  {
-    num: "08",
-    category: "Machine Learning",
-    title: "Movie Recommender",
-    description:
-      "Building the Movie Recommender system was my ticket into the wild world of machine learning. I cooked up a hybrid system that suggests movies like a psychic Netflix. Watching algorithms predict user preferences was a mind-blowing experience, and it seriously leveled up my Python and data skills. Plus, it’s got me thinking—AI might just be the future, and I’m here for it! So, Inception tonight?",
-    stack: [
-      { name: "Python" },
-      { name: "Jupyter Notebook" },
-      { name: "Pandas" },
-    ],
-    image: "/pop.png",
-    github: "https://github.com/naeem0512/MovieRecommender",
-  },
-  {
-    num: "09",
-    category: "Python",
-    title: "Snake Game",
-    description:
-      "Building the Snake Game with Pygame was a nostalgic trip back to my first mobile phone. Recreating that simple, addictive classic with Python was a blast, letting me dive into game mechanics and user experience. It was a fun way to flex my coding muscles and create something timeless.",
-    stack: [
-      { name: "Python" },
-      { name: "Pygame" },
-    ],
-    image: "/Terminal Game.png",
-    github: "https://github.com/naeem0512/snake-game",
-  },
+  //... other projects
 ];
 
 const Work = () => {
   const [project, setProject] = useState(projects[0]);
 
   const handleSlideChange = (swiper) => {
-    // get current slide index
     const currentIndex = swiper.activeIndex;
-    // update project state based on current slide index
     setProject(projects[currentIndex]);
   };
 
   return (
     <motion.section
       initial={{ opacity: 0 }}
-      animate={{
-        opacity: 1,
-        transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
-      }}
+      animate={{ opacity: 1, transition: { delay: 2.4, duration: 0.4, ease: "easeIn" } }}
       className="min-h-[80vh] flex flex-col justify-center py-12 xl:px-0"
     >
       <div className="container mx-auto">
         <div className="flex flex-col xl:flex-row xl:gap-[30px]">
           <div className="w-full xl:w-[50%] xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none">
             <div className="flex flex-col gap-[30px] h-[50%]">
-              {/* outline num */}
               <div className="text-8xl leading-none font-extrabold text-transparent text-outline">
                 {project.num}
               </div>
-              {/* project title */}
               <h2 className="text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500 capitalize">
                 {project.title}
               </h2>
-              {/* project description */}
               <p className="text-white/60">{project.description}</p>
-              {/* stack */}
               <ul className="flex gap-4">
-                {project.stack.map((item, index) => {
-                  return (
-                    <li key={index} className="text-xl text-accent">
-                      {item.name}
-                      {/* remove the last comma */}
-                      {index !== project.stack.length - 1 && ","}
-                    </li>
-                  );
-                })}
+                {project.stack.map((item, index) => (
+                  <li key={index} className="text-xl text-accent">
+                    {item.name}{index !== project.stack.length - 1 && ","}
+                  </li>
+                ))}
               </ul>
-              {/* border */}
               <div className="border border-white/20"></div>
               <div className="flex gap-4">
                 {/* Live demo button */}
@@ -219,18 +120,20 @@ const Work = () => {
                   </Link>
                 )}
                 {/* Github project button */}
-                <Link href={project.github}>
-                  <TooltipProvider delayDuration={100}>
-                    <Tooltip>
-                      <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
-                        <BsGithub className="text-white text-3xl group-hover:text-accent" />
-                      </TooltipTrigger>
-                      <TooltipContent>
-                        <p>Github repository</p>
-                      </TooltipContent>
-                    </Tooltip>
-                  </TooltipProvider>
-                </Link>
+                {project.github && (
+                  <Link href={project.github} target="_blank">
+                    <TooltipProvider delayDuration={100}>
+                      <Tooltip>
+                        <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
+                          <BsGithub className="text-white text-3xl group-hover:text-accent" />
+                        </TooltipTrigger>
+                        <TooltipContent>
+                          <p>Github repository</p>
+                        </TooltipContent>
+                      </Tooltip>
+                    </TooltipProvider>
+                  </Link>
+                )}
               </div>
             </div>
           </div>
@@ -241,27 +144,22 @@ const Work = () => {
               className="xl:h-[520px] mb-12"
               onSlideChange={handleSlideChange}
             >
-              {projects.map((project, index) => {
-                return (
-                  <SwiperSlide key={index} className="w-full">
-                    <div className="h-[460px] relative group flex justify-center items-center bg-pink-50/20">
-                      {/* overlay */}
-                      <div className="absolute top-0 bottom-0 w-full h-full bg-black/10 z-10"></div>
-                      {/* image */}
-                      <div className="relative w-full h-full">
-                        <Image
-                          src={project.image}
-                          fill
-                          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                          className="object-cover"
-                          alt={project.title}
-                        />
-                      </div>
+              {projects.map((project, index) => (
+                <SwiperSlide key={index} className="w-full">
+                  <div className="h-[460px] relative group flex justify-center items-center bg-pink-50/20">
+                    <div className="absolute top-0 bottom-0 w-full h-full bg-black/10 z-10"></div>
+                    <div className="relative w-full h-full">
+                      <Image
+                        src={project.image}
+                        fill
+                        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                        className="object-cover"
+                        alt={project.title}
+                      />
                     </div>
-                  </SwiperSlide>
-                );
-              })}
-              {/* slider buttons */}
+                  </div>
+                </SwiperSlide>
+              ))}
               <WorkSliderBtns
                 containerStyles="flex gap-2 absolute right-0 bottom-[calc(50%_-_22px)] xl:bottom-0 z-20 w-full justify-between xl:w-max xl:justify-none"
                 btnStyles="bg-accent hover:bg-accent-hover text-primary text-[22px] w-[44px] h-[44px] flex justify-center items-center transition-all"
